279d377d5644beeaca57de149b2ddae7865f8e69
eVookelj <evukelj@stuy.edu>
2016-01-13 09:44:46 -0500

    write MurderSituation

---
+++ MurderSituation.java
@@ -0,0 +1,29 @@
+public class MurderSituation {
+    private PersonCard _who;
+    private PlaceCard _where;
+    private WeaponCard _weapon;
+
+    public MurderSituation(PersonCard who, PlaceCard where, WeaponCard weapon) {
+	_who = who;
+	_where = where;
+	_weapon = weapon;
+    }
+
+    public PersonCard getWho() { return _who; }
+    public PlaceCard getWhere() { return _where; }
+    public WeaponCard getWeapon() { return _weapon; }
+
+    public boolean equals(Object o) {
+	if (o == this) { return true; }
+
+	if (o instanceof MurderSituation) {
+	    MurderSituation guess = (MurderSituation)o;
+	    if (_who.equals(guess._who) &&
+		_where.equals(guess._where) &&
+		_weapon.equals(guess._weapon)) {
+		return true;
+	    }
+	}
+	return false;
+    }
+}
