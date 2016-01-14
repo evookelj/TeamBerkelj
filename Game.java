8bc2f35a06ccbe9f7f5b66d64e1031988bbaf0e0
Wilson Berkow <wilsonberkow@gmail.com>
2016-01-14 09:46:13 -0500

    Start Game.java with basic config information

---
+++ Game.java
@@ -0,0 +1,29 @@
+public class Game {
+    public static String[] personNames = {
+        "Col. Mustard",
+        "Prof. Plum",
+        "Mr. Green",
+        "Mrs. Peacock",
+        "Miss Scarlet",
+        "Mrs. White"
+    };
+    public static String[] placeNames = {
+        "Hall",
+        "Lounge",
+        "Dining Room",
+        "Kitchen",
+        "Ball Room",
+        "Conservatory",
+        "Billiard Room",
+        "Library",
+        "Study"
+    };
+    public static String[] roomNames = {
+        "Knife",
+        "Candlestick",
+        "Revolver",
+        "Rope",
+        "Lead Pipe",
+        "Wrench"
+    };
+}
