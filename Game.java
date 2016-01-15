95e41b585ba25149c2ffeb49cce79c25d60c4ffe
Wilson Berkow <wilsonberkow@gmail.com>
2016-01-14 19:21:23 -0500

    Replace cards-config String values with Card objects

--- Game.java
+++ Game.java
@@ -1,29 +1,29 @@
 public class Game {
-    public static String[] personNames = {
-        "Col. Mustard",
-        "Prof. Plum",
-        "Mr. Green",
-        "Mrs. Peacock",
-        "Miss Scarlet",
-        "Mrs. White"
+    public static Card[] personCards = {
+        new Card("Col. Mustard", 0),
+        new Card("Prof. Plum", 0),
+        new Card("Mr. Green", 0),
+        new Card("Mrs. Peacock", 0),
+        new Card("Miss Scarlet", 0),
+        new Card("Mrs. White", 0)
     };
-    public static String[] placeNames = {
-        "Hall",
-        "Lounge",
-        "Dining Room",
-        "Kitchen",
-        "Ball Room",
-        "Conservatory",
-        "Billiard Room",
-        "Library",
-        "Study"
+    public static Card[] placeCards = {
+        new Card("Hall", 1),
+        new Card("Lounge", 1),
+        new Card("Dining Room", 1),
+        new Card("Kitchen", 1),
+        new Card("Ball Room", 1),
+        new Card("Conservatory", 1),
+        new Card("Billiard Room", 1),
+        new Card("Library", 1),
+        new Card("Study", 1)
     };
-    public static String[] roomNames = {
-        "Knife",
-        "Candlestick",
-        "Revolver",
-        "Rope",
-        "Lead Pipe",
-        "Wrench"
+    public static Card[] weaponCards = {
+        new Card("Knife", 2),
+        new Card("Candlestick", 2),
+        new Card("Revolver", 2),
+        new Card("Rope", 2),
+        new Card("Lead Pipe", 2),
+        new Card("Wrench", 2)
     };
 }
