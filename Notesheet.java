14a0a45c54986ed53deca79c15f3fe8cb3e665e4
eVookelj <evukelj@stuy.edu>
2016-01-16 09:44:57 -0500

    Finish initial versions of Notesheet and Notesheet Item. (Implementation of other classes may cause changes)

--- Notesheet.java
+++ Notesheet.java
@@ -1,17 +1,10 @@
+import java.util.*;
+
 public class Notesheet {
     private ArrayList<NotesheetItem> notes;
 
-    public String toString() {
-	String retStr = "";
-	for (int i=0; i<notes.size(); i++) {
-	    retStr += notes.get(i).getCard().getName() + ": "
-		+ notes.get(i).getIsInnocent() + "\n";
-	}
-	return retStr;
-    }
-
     public Notesheet() {
-	notes = new ArrayList<NotesheetItem>;
+	notes = new ArrayList<NotesheetItem>();
 	for (int i = 0; i < Game.personCards.length; i++) {
 	    notes.add(new NotesheetItem(Game.personCards[i], false));
 	}
@@ -23,5 +16,33 @@ public class Notesheet {
 	}
     }
 
-    public void makeW
+    public int indexOf(Card card) {
+	for (int i=0; i<notes.size(); i++) {
+	    if (notes.get(i).getCard().equals(card)) {
+		return i;
+	    }
+	    return -1;
+	}
+    }
+
+    public void crossOff(Card theInnocent) {
+	notes.get(indexOf(theInnocent)).setIsInnocent(true);
+    }
+
+    public String toString() {
+	String retStr = "";
+	retStr += "PEOPLE:\n";
+	for (int i=0; i<Game.personCards.length; i++) {
+	    retStr += notes.get(i).toString();
+	}
+	retStr += "\nPLACES:\n";
+	for (int i=Game.personCards.length; i<Game.placeCards.length; i++) {
+	    retStr += notes.get(i).toString();
+	}
+	retStr += "\nWEAPONS:\n";
+	for (int i=(Game.placeCards.length+Game.personCards.length); i<notes.size(); i++) {
+	    retStr += notes.get(i).toString();
+	}
+	return retStr;
+    }
 }
--- NotesheetItem.java
+++ NotesheetItem.java
@@ -14,4 +14,18 @@ public class NotesheetItem {
     public boolean getIsInnocent() {
 	return _isInnocent;
     }
+
+    public void setIsInnocent(boolean bool) {
+	_isInnocent = bool;
+    }
+
+    public String toString() {
+	String retStr = _card.getName() + ":\t";
+	if (_isInnocent) {
+	    retStr += "X";
+	} else {
+	    retStr += " ";
+	}
+	return retStr;
+    }
 }
